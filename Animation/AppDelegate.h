4d3f4067e29f019a256fa647eba3680e6da150de
RockeyFromZero <1309337847@qq.com>
2016-01-07 09:47:38 +0800

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by Rockey on 16/1/7.
+//  Copyright © 2016年 Rockey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by Rockey on 16/1/7.
+//  Copyright © 2016年 Rockey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
